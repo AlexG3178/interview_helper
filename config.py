454afa1702b2caf16509af7cd069859048a2ac0b
Alex <alexg3178@gmail.com>
2024-11-20 14:02:49 +0100

    hide openai api key

--- config.py
+++ config.py
@@ -1,8 +1,8 @@
+import os
+
 config = {
-    'api_key_openai': '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',
+    'api_key_openai': os.getenv('OPENAI_API_KEY', ''),
     'google_service_account_key': '/Users/alexandrgrigoriev/Documents/Projects/interview_helper/nth-celerity-442111-f4-ad0229c215b1.json',
-    # '/Users/alexandrgrigoriev/Documents/Projects/chromedriver-mac-x64/chromedriver'
-    'api_key_assemblyai': '7f2905e8b17043f0b8e30629cd7c9bc4',
     'MIN_SILENCE_THRESHOLD': 30,
     'SILENCE_PAUSE_DURATION': 2.0,
     'CHUNK': 2048,
